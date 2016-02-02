19c931c2e3303685e3c33dde33224e14fe4e70de
Shane Myrick <swmyrick@gmail.com>
2016-02-01 16:33:39 -0800

    Add Ant back in

--- bash-profile/expedia/expedia-environment.sh
+++ bash-profile/expedia/expedia-environment.sh
@@ -37,10 +37,14 @@
     #export JAVA_VERSION=1.8.0_25
     export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk$JAVA_VERSION.jdk/Contents/Home
 
+    # Ant Path
+    export ANT_VERSION=1.8.1
+    export ANT_HOME=$BUILD_SYSTEM_ROOT/buildsupport/apache-ant-$ANT_VERSION
+
     # Maven Path
     export MAVEN_VERSION="`mvn -v | grep 'Apache Maven' | ggrep -oP '\d\.\d\.\d'`"
     export MAVEN_HOME=/usr/local/Cellar/maven/$MAVEN_VERSION/libexec
     export M3_HOME=$MAVEN_HOME
 
     # Add to PATH
-    export PATH=$PATH:$JAVA_HOME/bin:$M3_HOME/bin
+    export PATH=$PATH:$JAVA_HOME/bin:$ANT_HOME/bin:$M3_HOME/bin
