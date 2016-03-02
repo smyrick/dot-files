7e1eb7f9678a0cd78d864a36bbb14c3ea2c5d368
Shane Myrick <swmyrick@gmail.com>
2016-03-02 14:28:36 -0800

    Maven version

--- bash-profile/expedia/expedia-environment.sh
+++ bash-profile/expedia/expedia-environment.sh
@@ -31,8 +31,8 @@
 # -------------------------------------------------------------------
 
     # Java Path
-    export JAVA_VERSION=1.7.0_60
-    #export JAVA_VERSION=1.8.0_25
+    #export JAVA_VERSION=1.7.0_60
+    export JAVA_VERSION=1.8.0_25
     export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk$JAVA_VERSION.jdk/Contents/Home
 
     # Ant Path
@@ -40,7 +40,7 @@
     export ANT_HOME=$BUILD_SYSTEM_ROOT/buildsupport/apache-ant-$ANT_VERSION
 
     # Maven Path
-    #export MAVEN_VERSION="`mvn -v | grep 'Apache Maven' | ggrep -oP '\d\.\d\.\d'`"
+    export MAVEN_VERSION="`mvn -v | grep 'Apache Maven' | ggrep -oP '\d\.\d\.\d'`"
     export MAVEN_HOME=/usr/local/Cellar/maven/$MAVEN_VERSION/libexec
     export M3_HOME=$MAVEN_HOME
 
