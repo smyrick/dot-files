88cf06d33371d2a8551952f4450387e565fca684
Shane Myrick <swmyrick@gmail.com>
2016-04-07 10:40:22 -0700

    Remove broken brew autocomplete

--- bash-profile/tools/auto-complete.sh
+++ bash-profile/tools/auto-complete.sh
@@ -4,8 +4,5 @@
 #   Auto Tab Complete
 #   -------------------------------
 
-    # HomeBrew autocomplete:
-    source $(brew --repository)/Library/Contributions/brew_bash_completion.sh
-
     # AWS CLI autocomplete:
     complete -C aws_completer aws
