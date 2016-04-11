7c403515101647255d1e05b18b6dd37bca299b6e
Shane Myrick <swmyrick@gmail.com>
2016-04-11 10:51:40 -0700

    Add changes

--- bash-profile/expedia/expedia-commands.sh
+++ bash-profile/expedia/expedia-commands.sh
@@ -19,14 +19,14 @@
     # ExpWeb Commands
     # -------------------------------------------------------------------
       TRUNK=$P4_1994/www/expweb/trunk
-      alias cd-expweb="cd $TRUNK"
+      alias cd-expweb="cd $TRUNK; clear;"
       alias gw="./gradlew"
       alias expweb-clean="sudo rm -rf $TRUNK/build"
       alias expweb-build="cd-expweb; expweb-clean; gw clean build"
       alias expweb-build-fast="cd-expweb; expweb-clean; gw clean build -xCheck -x minifyResources -Pdebug"
       alias expweb-start="cd-expweb; expweb-clean; gw -Pdebug startExpweb"
       alias expweb-start-stub="cd-expweb; expweb-clean; gw -Pdebug -Pstub startExpweb"
-      alias expweb-latest-version="p4 counters -e expweb_trunk-ci_last_green_cl | ggrep -oP '(\d)+'"
+      alias expweb-latest-version="cd-expweb; p4 counters -e expweb_trunk-ci_last_green_cl | ggrep -oP '(\d)+'"
       expweb-sync-latest() {
         eval "cd-expweb";
         p4 sync ${TRUNK}/...@$(expweb-latest-version);
