e278a6ac2c12350a9cfbd3a7328a148df063b875
Shane Myrick <swmyrick@gmail.com>
2016-04-15 15:23:57 -0700

    Remove unused variables

--- bash-profile/expedia/expedia-environment.sh
+++ bash-profile/expedia/expedia-environment.sh
@@ -8,14 +8,7 @@
 # Perforce file mappings
 # -------------------------------------------------------------------
     P4_ROOT=$HOME/src/perforce
-    P4_1953=$P4_ROOT/1953
-    P4_1967=$P4_ROOT/1967
     P4_1994=$P4_ROOT/1994
-    P4_1970=$P4_ROOT/1970
-    E3_SHOPSVC=$P4_1953/e3/components/shopsvc
-    E3_MODULES=$E3_SHOPSVC/modules
-    E3_PRODUCTS=$E3_SHOPSVC/products
-    E3_PROTOTYPES=$E3_SHOPSVC/prototypes/modules
 
     # Perforce ExpWeb configs
     export P4CONFIG=p4.ini
