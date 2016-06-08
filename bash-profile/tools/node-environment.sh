c2c1e26b773de2b12b11b9fff69e82e676cef53c
Shane Myrick <swmyrick@gmail.com>
2016-06-08 14:50:20 -0700

    Add rvm to node environment

--- bash-profile/tools/node-environment.sh
+++ bash-profile/tools/node-environment.sh
@@ -3,10 +3,21 @@
 #   -------------------------------
 #   Node Environment
 #   -------------------------------
-#
+
+    #
+    # Load NVM
+    #
     export NVM_DIR="$HOME/.nvm"
     . "$(brew --prefix nvm)/nvm.sh"
 
+    #
+    # Load RVM into a shell session *as a function*
+    #
+    source $HOME/.rvm/scripts/rvm;
+
+    #
+    # Set npm install directory
+    #
     NPM_PACKAGES="$HOME/.npm-packages"
     NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
     export PATH=$NPM_PACKAGES/bin:$PATH
