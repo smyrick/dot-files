08a2451a7981ec5098aa5bf805c21acb040d30f0
Shane Myrick <swmyrick@gmail.com>
2016-07-07 17:44:44 -0700

    Java version

--- bash-profile/expedia/expedia-environment.sh
+++ bash-profile/expedia/expedia-environment.sh
@@ -43,8 +43,8 @@
 # -------------------------------------------------------------------
 
     # Java Path
-    export JAVA_VERSION=1.7.0_60
-    #export JAVA_VERSION=1.8.0_74
+    #export JAVA_VERSION=1.7.0_60
+    export JAVA_VERSION=1.8.0_74
     export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk$JAVA_VERSION.jdk/Contents/Home
 
     # Maven Path
