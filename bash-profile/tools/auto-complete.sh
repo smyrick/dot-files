ccd0d22e0a583bcfdd5c48870850d5eb5a22762b
Shane Myrick <swmyrick@gmail.com>
2016-11-07 14:03:58 -0800

    Add bash completion from homebrew

--- bash-profile/tools/auto-complete.sh
+++ bash-profile/tools/auto-complete.sh
@@ -6,3 +6,8 @@
 
     # AWS CLI autocomplete:
     complete -C aws_completer aws
+
+    # Bash completion
+    if [ -f $(brew --prefix)/etc/bash_completion ]; then
+        . $(brew --prefix)/etc/bash_completion
+    fi
